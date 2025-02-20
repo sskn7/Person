94593d6d75788e815aba1f5fad76c2ea86dd0c38
kana sasazawa <kana.yellow7@gmail.com>
2025-02-20 22:27:34 +0900

    演習２

--- src/jp/co/diworks/action/Test.java
+++ src/jp/co/diworks/action/Test.java
@@ -8,8 +8,15 @@ public class Test {
 		taro.name = "山田太郎";
 		taro.age = 20;
 		
+		Person jiro = new Person();
+		jiro.name = "木村次郎";
+		jiro.age = 18;
+		
 		System.out.println(taro.name);
 		System.out.println(taro.age);
+		
+		System.out.println(jiro.name);
+		System.out.println(jiro.age);
 	}
 
 }
